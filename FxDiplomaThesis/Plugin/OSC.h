dc5b8b076890d439c1ba6652603c82fd2851cf82
kastre <92887497+kastre3381@users.noreply.github.com>
2024-12-15 22:34:55 +0100

    Added key events and OSC

---
+++ FxDiplomaThesis/Plugin/OSC.h
@@ -0,0 +1,16 @@
+//
+//  OSC.h
+//  FxDiplomaThesis
+//
+//  Created by Kacper  on 15/12/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OSC : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FxDiplomaThesis/Plugin/OSCShader.metal
@@ -0,0 +1,11 @@
+//
+//  OSCShader.metal
+//  FxDiplomaThesis
+//
+//  Created by Kacper  on 13/12/2024.
+//
+
+#include <metal_stdlib>
+using namespace metal;
+
+
